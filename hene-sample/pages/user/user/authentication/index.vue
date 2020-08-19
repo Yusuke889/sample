f06c991c0560fceb3f06d517b37d42a672a24a1c
yusuke-kita <>
2020-08-19 19:26:23 +0900

    ユーザー作成まで

--- hene-sample/pages/user/user/authentication/index.vue
+++ hene-sample/pages/user/user/authentication/index.vue
@@ -19,7 +19,7 @@
           <v-text-field label="認証番号6桁を記入してください" />
         </v-card-actions>
         <v-card-actions>
-          <v-btn to="/user/user/authentication">SMS認証</v-btn>
+          <v-btn to="/user/user/create">SMS認証</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
---
+++ hene-sample/pages/user/user/create/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card
+        min-width="300px"
+      >
+        <v-card-title class="headline">
+          新規会員会員登録
+        </v-card-title>
+        <v-card-actions>
+          <v-text-field label="名前" />
+        </v-card-actions>
+        <v-card-actions>
+          <v-text-field label="電話番号" />
+        </v-card-actions>
+        <v-card-actions>
+          <v-text-field label="メールアドレス" />
+        </v-card-actions>
+        <v-card-actions>
+          <v-text-field label="性別" />
+        </v-card-actions>
+        <v-card-actions>
+          <v-text-field label="パスワード" />
+        </v-card-actions>
+        <v-card-actions>
+          <v-text-field label="確認用パスワード" />
+        </v-card-actions>
+        <v-card-actions>
+          <v-btn to="/user/mypage">アカウント登録</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+    export default {
+        layout: 'userDefault',
+    }
+</script>
