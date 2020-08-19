80f065fc9b5db5cdb62528b7f25c693016d623a2
yusuke-kita <>
2020-08-19 19:00:40 +0900

    ユーザー側ログインページ

--- hene-sample/layouts/userDefault.vue
+++ hene-sample/layouts/userDefault.vue
@@ -1,40 +1,28 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title"/>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
     >
-      <!--      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />-->
-      <v-toolbar-title v-text="title"/>
+      <v-toolbar-title v-text="'ユーザー側Web'" />
+      <v-spacer />
+      <v-btn
+        icon
+        to="/user/tel"
+      >
+        <v-icon>mdi-login-variant</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        to="/user/login"
+      >
+        <v-icon>mdi-human-edit</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
-        <nuxt/>
+        <nuxt />
       </v-container>
     </v-main>
     <v-footer
@@ -48,21 +36,26 @@
 
 <script>
     export default {
-        name: 'userDefault',
-        data() {
+        data () {
             return {
                 clipped: false,
-                drawer: true,
+                drawer: false,
                 fixed: false,
                 items: [
                     {
-                        icon: 'mdi-shopping',
-                        title: '店舗一覧',
-                        to: '/admin/shop/'
+                        icon: 'mdi-apps',
+                        title: 'Welcome',
+                        to: '/'
                     },
+                    {
+                        icon: 'mdi-chart-bubble',
+                        title: 'Inspire',
+                        to: '/inspire'
+                    }
                 ],
                 miniVariant: false,
-                title: '顔認証システム'
+                right: true,
+                rightDrawer: false,
             }
         }
     }
---
+++ hene-sample/pages/user/login/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card>
+        <v-card-title class="headline">
+          ログイン
+        </v-card-title>
+        <v-card-actions>
+        <v-text-field label="メールアドレス" />
+        </v-card-actions>
+        <v-card-actions>
+        <v-text-field append-icon="mdi-eye-off" label="パスワード" />
+        </v-card-actions>
+        <v-card-actions>
+          <v-btn>ログイン</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+    import Logo from '~/components/Logo.vue'
+    import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+    export default {
+        layout: 'userDefault',
+        name: 'userDefaultIndex',
+        components: {
+            Logo,
+            VuetifyLogo
+        }
+    }
+</script>
