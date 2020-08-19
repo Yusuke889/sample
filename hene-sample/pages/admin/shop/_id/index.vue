cfe6b59603611521614370efb47ea93aab8a836d
yusuke-kita <>
2020-08-19 14:50:04 +0900

    店舗一覧と詳細を作成

--- hene-sample/pages/admin/shop/_id/index.vue
+++ hene-sample/pages/admin/shop/_id/index.vue
@@ -1,83 +1,165 @@
 <template>
   <div>
-
-
-  <v-form>
-    <v-card
-      class="mx-auto"
-      max-width="800"
-      outlined
-    >
-      <v-card-title>パスワードの再設定</v-card-title>
-      <v-card-text>
-        <v-text-field
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show1 ? 'password' : 'text'"
-          name="input-10-1"
-          label="新しいパスワードを入力してください"
-          counter
-          @click:append="show1 = !show1"
-        ></v-text-field>
-        <v-text-field
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show1 ? 'password' : 'text'"
-          name="input-10-1"
-          label="確認用のパスワードを入力してください"
-          counter
-          @click:append="show1 = !show1"
-        ></v-text-field>
-
-        <v-card-actions>
-          <v-btn
-            color="primary"
-            dark
-            @click="snackbar = true"
-          >パスワードを新しく設定する</v-btn>
-        </v-card-actions>
-      </v-card-text>
-    </v-card>
-    </v-form>
-
-    <v-snackbar
-      v-model="snackbar"
-      :bottom="y === 'bottom'"
-      :color="color"
-      :left="x === 'left'"
-      :multi-line="mode === 'multi-line'"
-      :right="x === 'right'"
-      :timeout="timeout"
-      :top="y === 'top'"
-      :vertical="mode === 'vertical'"
-    >
-      {{ text }}
-      <template v-slot:action="{ attrs }">
-        <v-btn
-          dark
-          text
-          v-bind="attrs"
-          @click="snackbar = false"
-          to="/admin/login"
+        <v-card
+          outlined
         >
-          ログイン画面へ
-        </v-btn>
-      </template>
-    </v-snackbar>
+          <v-card-title>店舗詳細
+            <v-spacer/>
+            <v-btn
+              color="error"
+              @click="deleteConfirm"
+            >
+              アカウントを停止する
+            </v-btn>
+          </v-card-title>
+          <v-card-text>
+              <v-text-field
+                value="スキー場運営店舗"
+                label="名前"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value="08011119999"
+                label="電話番号"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value="akasaka@example.com"
+                label="メールアドレス"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value="店舗担当者氏名"
+                label="担当者"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value="09011112222"
+                label="担当者担当者電話番号"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value="https://google.com"
+                label="ホームページURL"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value="承認済み"
+                label="ステータス"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value="2020年10月20日 13時59分"
+                label="申請承認日時"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value=" "
+                label="退会日時"
+                readonly
+              ></v-text-field>
+
+              <v-text-field
+                value=" "
+                label="アカウント停止日時"
+                readonly
+              ></v-text-field>
+          </v-card-text>
+        </v-card>
+
+    <!--  モーダル  -->
+    <v-row justify="center">
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-card min-height="175px">
+          <v-card-title>
+            <h3>本当にこのアカウントを停止してよろしいですか？<br>このユーザーはサービスを利用できなくなります</h3>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn color="error" @click="deleted" class="mr-auto">停止</v-btn>
+          </v-card-actions>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">閉じる</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+
   </div>
 </template>
 
 <script>
     export default {
-        data () {
+        data() {
             return {
-                show1: true,
-                color: '',
-                mode: '',
-                snackbar: false,
-                text: 'パスワードの変更を完了しました',
-                timeout: 6000,
-                x: null,
-                y: 'top',
+                dialog: false,
+                headers: [
+                    {
+                        text: '利用店舗',
+                        value: 'name',
+                    },
+                    {text: '利用日時', value: 'theDateOfUse'},
+                    {text: 'お支払い金額', value: 'amount'},
+                ],
+                events: [
+                    {
+                        name: 'イベント会場',
+                        theDateOfUse: '2020年12月3日 19時3分',
+                        amount: '8,000円',
+                    },
+                    {
+                        name: 'ホテル',
+                        theDateOfUse: '2020年12月2日 17時10分',
+                        amount: '35,000円',
+                    },
+                    {
+                        name: 'ゴルフ場',
+                        theDateOfUse: '2020年12月2日 9時5分',
+                        amount: '10,000円',
+                    },
+                    {
+                        name: 'スキー会場',
+                        theDateOfUse: '2020年11月2日 13時16分',
+                        amount: '10,000円',
+                    },
+                    {
+                        name: '旅館',
+                        theDateOfUse: '2020年11月12日 11時22分',
+                        amount: '20,000円',
+                    },
+                    {
+                        name: 'プール',
+                        theDateOfUse: '2020年9月2日 10時55分',
+                        amount: '2,000円',
+                    },
+                ]
+            }
+        },
+        methods: {
+            update() {
+                this.updateSnackbar = true
+            },
+            deleteConfirm() {
+                this.dialog = true
+            },
+            deleted() {
+                this.dialog = false
+                this.$router.push('/admin/admin')
             }
         }
     }
 </script>
+
+<style scoped>
+  .mr-auto {
+    margin: auto;
+  }
+</style>
--- hene-sample/pages/admin/shop/index.vue
+++ hene-sample/pages/admin/shop/index.vue
@@ -1,83 +1,161 @@
 <template>
   <div>
-
-
-  <v-form>
     <v-card
-      class="mx-auto"
-      max-width="800"
       outlined
     >
-      <v-card-title>パスワードの再設定</v-card-title>
-      <v-card-text>
+      <v-card-title>
+        店舗一覧
+        <v-spacer />
         <v-text-field
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show1 ? 'password' : 'text'"
-          name="input-10-1"
-          label="新しいパスワードを入力してください"
-          counter
-          @click:append="show1 = !show1"
-        ></v-text-field>
+          v-model="searchText"
+          label="店舗名検索"
+          sigle-line
+        />
+        <v-spacer />
         <v-text-field
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show1 ? 'password' : 'text'"
-          name="input-10-1"
-          label="確認用のパスワードを入力してください"
-          counter
-          @click:append="show1 = !show1"
-        ></v-text-field>
-
-        <v-card-actions>
-          <v-btn
-            color="primary"
-            dark
-            @click="snackbar = true"
-          >パスワードを新しく設定する</v-btn>
-        </v-card-actions>
-      </v-card-text>
-    </v-card>
-    </v-form>
-
-    <v-snackbar
-      v-model="snackbar"
-      :bottom="y === 'bottom'"
-      :color="color"
-      :left="x === 'left'"
-      :multi-line="mode === 'multi-line'"
-      :right="x === 'right'"
-      :timeout="timeout"
-      :top="y === 'top'"
-      :vertical="mode === 'vertical'"
-    >
-      {{ text }}
-      <template v-slot:action="{ attrs }">
+          v-model="searchText"
+          label="電話番号"
+          sigle-line
+        />
+        <v-spacer />
+        <v-select
+          v-model="value"
+          :items="selectItems"
+          attach
+          chips
+          label="承認ステータス"
+          multiple
+          class="mw-100"
+        ></v-select>
+        <v-spacer />
         <v-btn
-          dark
-          text
-          v-bind="attrs"
-          @click="snackbar = false"
-          to="/admin/login"
+          color="success"
+          class="mr-4"
         >
-          ログイン画面へ
+          検索
+        </v-btn>
+        <v-spacer />
+        <v-btn
+          color="secondary"
+          class="mr-4"
+        >
+          CSVダウンロード
+        </v-btn>
+      </v-card-title>
+    </v-card>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :page.sync="page"
+      :items-per-page="itemsPerPage"
+      @page-count="pageCount = $event"
+      item-key="name"
+      hide-default-footer
+      class="elevation-1 row-pointer"
+      @click:row="onClickEvent"
+    >
+      <template v-slot:item.credit="{ item }">
+        {{ item.credit ? '済' :'未'}}
+      </template>
+      <template v-slot:item.detail="{ item }">
+        <v-btn
+          color="primary"
+          to="/admin/shop/1"
+        >
+          詳細
         </v-btn>
       </template>
-    </v-snackbar>
+    </v-data-table>
+    <div class="text-center pt-2">
+      <v-pagination v-model="page" :length="pageCount"></v-pagination>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
-        data () {
+        data() {
             return {
+                selectItems: ['未承認', '承認済み'],
+                dialog: false,
+                snackbar: false,
                 show1: true,
                 color: '',
                 mode: '',
-                snackbar: false,
                 text: 'パスワードの変更を完了しました',
                 timeout: 6000,
                 x: null,
                 y: 'top',
+                headers: [
+                    {text: '店舗名', value: 'name',},
+                    {text: '電話番号', value: 'tel'},
+                    {text: '担当者様氏名', value: 'person'},
+                    {text: '承認ステータス', value: 'status'},
+                    {text: '詳細', value: 'detail'},
+                ],
+                items: [
+                    {
+                        name: 'イベント会場',
+                        tel: '09089924708',
+                        person: '鈴木理恵',
+                        status: '未申請',
+                    },
+                    {
+                        name: 'ホテル',
+                        tel: '09048709933',
+                        person: '多田 秀次郎',
+                        status: '申請中',
+                    },
+                    {
+                        name: 'ゴルフ場',
+                        tel: '09048709933',
+                        person: '中田 孝彦',
+                        status: '否認',
+                    },
+                    {
+                        name: 'スキー会場',
+                        tel: '09048709933',
+                        person: '2020-01-01 21:00:00',
+                        status: '承認済み',
+                    },
+                    {
+                        name: '旅館',
+                        tel: '09048709933',
+                        person: '細川 徳彦',
+                        status: 'アカウント停止',
+                    },
+                    {
+                        name: 'プール',
+                        tel: '09048709933',
+                        person: '川島 あさ美',
+                        status: '承認済み',
+                    },
+                ],
+                page: 1,
+                pageCount: 1,
+                itemsPerPage: 10,
+            }
+        },
+        methods: {
+            onClickEvent() {
+                this.$router.push(`/admin/shop/1`)
+            },
+            /**
+             * 招待時の処理
+             */
+            invitaiton() {
+                this.dialog = false
+                this.snackbar = true
             }
         }
     }
 </script>
+
+<style lang="css" scoped>
+  .row-pointer >>> tbody tr :hover {
+    cursor: pointer;
+  }
+  .mw-100 {
+    max-width: 300px;
+  }
+</style>
